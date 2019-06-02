4cd980bc00c0af88a5c59652e83967cefd0dbfdf
s-sawaguchi <sawaguchi0604@gmail.com>
2019-06-02 14:44:50 +0900

    Add Project

---
+++ Assets/ゲーム/スクリプト/Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private UIScript userInterface;
+
+    Animator _anim;
+    AudioSource _audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        userInterface = GameObject.FindObjectOfType<UIScript>();
+        _anim = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pickup") {
+
+            if (userInterface != null) {
+                // add one point
+                userInterface.AddPoints(0, 1);
+            }
+            _anim.SetTrigger("Happy");
+
+            _audio.Play();
+            // then destroy this object
+            Destroy(collision.gameObject);
+        }
+    }
+   
+}
---
+++ Assets/ゲーム/スクリプト/Game.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] GameObject _particle;
+    [SerializeField] UIScript _uiScript;
+
+    AudioSource _audio;
+    bool _gameEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameEnd = false;
+        _audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_uiScript.GameClearFlag && !_gameEnd) {
+            Instantiate(_particle);
+            _gameEnd = true;
+            _audio.Play();
+        }
+    }
+
+    
+}
